b89497d3a79b5051abad0d5d190b28875cd4b4a5
Curtion <curtion@126.com>
2025-01-09 14:41:44 +0800

    feat: 支持拖拽和粘贴的二维码生成

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -16,9 +16,64 @@ function handleInputChange() {
   // TODO 可以在这里设置isLocked状态, 用于锁定当前标签页的url
 }
 
+function handleDataTransferItem(list: DataTransferItem[]) {
+  const fileItems = list.find(item => item.kind === 'file')
+  if (fileItems) {
+    const file = fileItems.getAsFile()
+    if (file) {
+      console.log('拖拽的文件:', file)
+      // TODO: 处理文件
+    }
+  } else {
+    const textItems = list.find(item => item.kind === 'string' && item.type === 'text/plain')
+    if (!textItems) {
+      return
+    }
+    textItems.getAsString((text) => {
+      tabs.value[currentTabId.value].url = text
+    })
+  }
+}
+
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
 }
+
+function handleDragOver(event: DragEvent) {
+  event.preventDefault()
+}
+
+function handleDrop(event: DragEvent) {
+  event.preventDefault()
+  const dt = event.dataTransfer
+  if (!dt?.items) {
+    return
+  }
+  const list = Array.from(dt.items)
+  handleDataTransferItem(list)
+}
+
+function handlePaste(event: ClipboardEvent) {
+  event.preventDefault()
+  const items = event.clipboardData?.items
+  if (!items) {
+    return
+  }
+  const list = Array.from(items)
+  handleDataTransferItem(list)
+}
+
+onMounted(() => {
+  document.addEventListener('dragover', handleDragOver)
+  document.addEventListener('drop', handleDrop)
+  document.addEventListener('paste', handlePaste)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('dragover', handleDragOver)
+  document.removeEventListener('drop', handleDrop)
+  document.removeEventListener('paste', handlePaste)
+})
 </script>
 
 <template>
@@ -39,5 +94,12 @@ function openOptionsPage() {
     <button class="btn mt-2" @click="openOptionsPage">
       打开设置页
     </button>
+    <div class="text-sm text-gray-500 mt-4 px-4 py-2 bg-gray-100 rounded-md">
+      可以通过以下方式快速操作：
+      <ul class="mt-1 list-disc list-inside">
+        <li>拖拽二维码图片/文字到侧边栏</li>
+        <li>聚焦侧边栏, 按 Ctrl+V (Mac: Command+V) 粘贴图片/文字</li>
+      </ul>
+    </div>
   </main>
 </template>
