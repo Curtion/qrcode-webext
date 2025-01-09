1d87a38edbfca7c9a811846c9f7bbba4ddfcba05
Curtion <curtion@126.com>
2025-01-09 11:42:47 +0800

    feat: 保存手动修改的记录

--- src/logic/tab.ts
+++ src/logic/tab.ts
@@ -1,18 +1,44 @@
 import type { ChannelData } from '~/types/channel'
 
-const currentUrl = ref<undefined | string>()
+const currentTabId = ref<number>(0)
 const currentWindowId = ref<undefined | number>()
+const tabs = ref<{
+  [tabId: number]: {
+    url?: string
+    islocked: boolean
+  }
+}>({})
 
 browser.runtime.onMessage.addListener(async (message) => {
   const data = message as ChannelData
   if (data.type === 'onActivated') {
     if (data.data.windowId === currentWindowId.value) {
-      currentUrl.value = data.data.tab.url
+      const tabId = data.data.tab.id
+      if (tabId && !tabs.value[tabId]) {
+        tabs.value[tabId] = {
+          url: data.data.tab.url,
+          islocked: false,
+        }
+      }
+      currentTabId.value = tabId ?? 0
     }
   } else if (data.type === 'onUpdated') {
     if (data.data.changeInfo.status === 'complete' && data.data.tab.active) {
       if (data.data.tab.windowId === currentWindowId.value) {
-        currentUrl.value = data.data.tab.url
+        const tabId = data.data.tab.id
+        if (tabId) {
+          if (!tabs.value[tabId]) {
+            tabs.value[tabId] = {
+              url: data.data.tab.url,
+              islocked: false,
+            }
+          } else {
+            if (tabs.value[tabId].islocked) {
+              return
+            }
+            tabs.value[tabId].url = data.data.tab.url
+          }
+        }
       }
     }
   }
@@ -21,8 +47,14 @@ browser.runtime.onMessage.addListener(async (message) => {
 async function init() {
   const [tab] = await browser.tabs.query({ active: true, currentWindow: true })
   currentWindowId.value = tab.windowId
-  currentUrl.value = tab.url
+  if (tab.id) {
+    currentTabId.value = tab.id
+    tabs.value[tab.id] = {
+      url: tab.url,
+      islocked: false,
+    }
+  }
 }
 init()
 
-export { currentUrl }
+export { currentTabId, tabs }
--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-import { currentUrl } from '~/logic/tab'
+import { currentTabId, tabs } from '~/logic/tab'
 import { generateQR } from '~/logic/qrcode'
 
 const qrdata = ref<undefined | string>()
 
 watchEffect(async () => {
-  if (!currentUrl.value) {
+  const url = tabs.value[currentTabId.value]?.url
+  if (!url) {
     return
   }
-  qrdata.value = await generateQR(currentUrl.value)
+  qrdata.value = await generateQR(url)
 })
+
+function handleInputChange() {
+  console.log('handleInputChange')
+}
 
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
@@ -20,7 +25,8 @@ function openOptionsPage() {
   <main class="w-full h-screen px-4 py-5 text-gray-700 bg-[#F7F7F7] flex items-center flex-col">
     <img :src="qrdata" alt="二维码">
     <textarea
-      v-model="currentUrl" class="w-full min-h-50 mt-2 p-2 text-sm text-gray-700 bg-white
+      v-model="tabs[currentTabId].url"
+      class="w-full min-h-50 mt-2 p-2 text-sm text-gray-700 bg-white
          rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500
          outline-none transition duration-200 hover:border-blue-400
          resize-y
@@ -28,6 +34,7 @@ function openOptionsPage() {
          scrollbar-track:bg-gray-100 dark:scrollbar-track:bg-gray-700
          scrollbar-thumb:bg-gray-300 dark:scrollbar-thumb:bg-gray-600
          hover:scrollbar-thumb:bg-gray-400"
+      @change="handleInputChange"
     />
     <button class="btn mt-2" @click="openOptionsPage">
       打开设置页
